86d94ed52654340583547a41d56ea41cbb782f06
shivero <sh1veeer@gmail.com>
2023-12-27 18:30:15 +0100

    Refactor Avatar and MemberList components, add AvatarStatus component, and update Messages component

--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { useMemberPanelStore } from "@/store/memberPanelStore";
-import { getGroupedMembers } from "../data/members";
-const showMemberPanel = true;
-
+import { getGroupedMembers } from "@/data/members";
+import Avatar from "./Avatar.vue";
+import AvatarStatus from "./AvatarStatus.vue";
 const memberPanelStore = useMemberPanelStore();
-
 
 const colorMemberNameByRole = (role: string) => {
   switch (role) {
@@ -19,20 +18,20 @@ const colorMemberNameByRole = (role: string) => {
   }
 };
 </script>
-
 <template>
   <div
     v-show="memberPanelStore.isOpen"
     class="scrollbar flex min-h-0 w-60 flex-col gap-2 overflow-y-scroll bg-discord-dark-4 px-4">
-    <div v-for="(member, gindex) in getGroupedMembers()" class="">
+    <div v-for="(member, gindex) in getGroupedMembers()">
       <div class="mt-6 text-[10px] font-semibold uppercase tracking-wider text-gray-400">
         {{ gindex }} – {{ member.length }}
       </div>
       <div
         v-for="(item, index) in member"
         class="flex items-center gap-4 py-2 text-gray-400">
-        <div class="h-10 w-10 overflow-hidden rounded-full bg-gray-600">
-          <img :src="item.avatar_url" alt="" />
+        <div class="relative">
+          <Avatar class="h-9 w-9" />
+          <AvatarStatus :status="item.status" class="absolute -bottom-1 right-0 border-2 rounded-full border-discord-dark-4" />
         </div>
         <div :class="colorMemberNameByRole(gindex as string)">
           {{ item.name }}
